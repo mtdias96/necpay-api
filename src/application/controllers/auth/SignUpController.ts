239004a81f93a5356f33fc22a87e6fadd089c873
mtdias96 <matheusdias.front96@gmail.com>
2025-06-04 18:54:28 -0300

    feat: add signup controller, usecase and schema

---
+++ src/application/controllers/auth/SignUpController.ts
@@ -0,0 +1,37 @@
+import { Controller } from '@application/contracts/Controller';
+
+import { SignUpUseCase } from '@application/usecases/auth/SignUpUseCase';
+import { Injectable } from '@kernel/decorators/Injectable';
+import { Schema } from '@kernel/decorators/Schema';
+import { SignUpBody, signUpSchema } from './schemas/signUpController';
+
+@Injectable()
+@Schema(signUpSchema)
+export class SignUpController extends Controller<SignUpController.Response> {
+  constructor(private readonly signupUseCase: SignUpUseCase) {
+    super();
+  }
+  protected override async handle({ body }: Controller.Request<SignUpBody>): Promise<Controller.Response<SignUpController.Response>> {
+    const { account } = body;
+
+    const {
+      accessToken,
+      refreshToken,
+    } = await this.signupUseCase.execute(account);
+
+    return {
+      statusCode: 201,
+      body: {
+        accessToken,
+        refreshToken,
+      },
+    };
+  }
+}
+
+export namespace SignUpController {
+  export type Response = {
+    accessToken: string;
+    refreshToken: string;
+  }
+}
