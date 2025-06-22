97aafd09249a0b6257b593417ce4cca6c78a65c6
mtdias96 <matheusdias.front96@gmail.com>
2025-06-22 19:21:18 -0300

    fix: alter path folder

--- src/main/functions/auth/preSignUpTrigger.ts
+++
@@ -1,8 +0,0 @@
-import { PreSignUpTriggerEvent } from 'aws-lambda';
-
-export async function handler(event: PreSignUpTriggerEvent) {
-  event.response.autoConfirmUser = true;
-  event.response.autoVerifyEmail = true;
-
-  return event;
-}
