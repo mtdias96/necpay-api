c766d35cebe84be38a108c876b36c990992c9796
mtdias96 <matheusdias.front96@gmail.com>
2025-06-25 23:24:44 -0300

    feat: add new method findByEmail

--- src/infra/database/drizzle/repositories/AccountRepository.ts
+++ src/infra/database/drizzle/repositories/AccountRepository.ts
@@ -1,19 +1,25 @@
 import { Account } from '@application/entities/Account';
+import { DrizzleClient } from '@infra/clients/drizzleClient';
 import { Injectable } from '@kernel/decorators/Injectable';
-import { AccountMapper } from '../mappers/AccountMapper';
+import { eq } from 'drizzle-orm';
 import { accountsTable } from '../schema/accounts';
 import { UnitOfWorkTransaction } from '../types/UnitOfWorkTransaction';
 
 @Injectable()
 export class AccountRepository {
-  constructor() { }
+  constructor(private readonly db: DrizzleClient) { }
+
+  async findByEmail(email: string): Promise<Account | null> {
+    const result = await this.db.httpClient.select().from(accountsTable).where(eq(accountsTable.email, email));
+
+    return result[0];
+  }
 
   async create(
     account: Account,
     trx: UnitOfWorkTransaction,
   ): Promise<void> {
     const transaction = trx;
-    const data = AccountMapper.toPersistence(account);
-    await transaction.insert(accountsTable).values(data);
+    await transaction.insert(accountsTable).values(account);
   }
 }
