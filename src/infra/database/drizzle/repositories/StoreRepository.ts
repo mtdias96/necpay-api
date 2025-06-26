366403c0a0a4e576c09d8eb8451ec02d71e4c470
mtdias96 <matheusdias.front96@gmail.com>
2025-06-25 23:24:32 -0300

    feat: add new method findByName

--- src/infra/database/drizzle/repositories/StoreRepository.ts
+++ src/infra/database/drizzle/repositories/StoreRepository.ts
@@ -1,19 +1,30 @@
 import { Store } from '@application/entities/Store';
+import { DrizzleClient } from '@infra/clients/drizzleClient';
 import { Injectable } from '@kernel/decorators/Injectable';
-import { StoreMapper } from '../mappers/StoreMapper';
+import { eq } from 'drizzle-orm';
 import { storesTable } from '../schema/stores';
 import { UnitOfWorkTransaction } from '../types/UnitOfWorkTransaction';
 
 @Injectable()
 export class StoreRepository {
-  constructor() { }
+  constructor(private readonly db: DrizzleClient) { }
+
+  async findByName(name: string): Promise<
+    { id: string; name: string } | null
+  > {
+    const result = await this.db.httpClient
+      .select({ id: storesTable.id, name: storesTable.name })
+      .from(storesTable)
+      .where(eq(storesTable.name, name));
+
+    return result[0] ?? null;
+  }
 
   async create(
     store: Store,
     trx: UnitOfWorkTransaction,
   ): Promise<void> {
     const transaction = trx;
-    const data = StoreMapper.toPersistence(store);
-    await transaction.insert(storesTable).values(data);
+    await transaction.insert(storesTable).values(store);
   }
 }
