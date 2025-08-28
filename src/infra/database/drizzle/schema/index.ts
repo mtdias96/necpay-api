41d4c18928cc9ebd74c54ce4c635f21303d0fccf
mtdias96 <matheusdias.front96@gmail.com>
2025-08-28 20:25:31 -0300

    feat: add categoriesTable to schema export

--- src/infra/database/drizzle/schema/index.ts
+++ src/infra/database/drizzle/schema/index.ts
@@ -1,11 +1,19 @@
 import { accountsTable } from './accounts';
+import { categoriesTable } from './categories';
 import { stockMovementsTable } from './stock';
 import { storesTable } from './stores';
+
+export * from '../schema';
+export * from './accounts';
+export * from './categories';
+export * from './orders';
+export * from './product';
 
 export const schema = {
   accountsTable,
   storesTable,
   stockMovementsTable,
+  categoriesTable,
 };
 
 export type Schema = typeof schema;
